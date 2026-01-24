71b032da0dc5415ac359a49e0b4bff88edd21f5c
Zhantore Bekberdiev <z_bekberdiev@icloud.com>
2026-01-24 17:19:07 +0500

    Added 'syntax.py' file.

---
+++ practice-1/python-basics/syntax.py
@@ -0,0 +1,23 @@
+if 5 > 2:
+  print("Five is greater than two!") # Indentation: 2 spaces
+
+if 5 > 2:
+        print("Five is greater than two!") # Indentation: 2 tabs
+
+# Variables' declaration and initialization at the same time
+x = 5
+y = "Hello, World!"
+
+# This is a comment
+print("Hello, World!")
+
+# One statement
+print("Python is fun!")
+
+# Several statements
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+# Semicolons separate several statements written in one line
+print("Hello"); print("How are you?"); print("Bye bye!")
