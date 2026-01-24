e0f1515a0d1bbdeb286c7eda91fcff92a733b2d8
Zhantore Bekberdiev <z_bekberdiev@icloud.com>
2026-01-24 17:21:15 +0500

    Added 'output.py' file.

---
+++ practice-1/python-basics/output.py
@@ -0,0 +1,7 @@
+# Usage of single or double quotes
+print("This will work!")
+print('This will also work!')
+
+# Output without a new line
+print("Hello World!", end=" ")
+print("I will print on the same line.")
