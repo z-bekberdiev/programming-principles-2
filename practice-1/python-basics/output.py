92e08f9c2c1335e0eb6753431ac658917c7cd173
Zhantore Bekberdiev <z_bekberdiev@icloud.com>
2026-01-24 17:25:31 +0500

    Added some changes into 'output.py' file.

--- practice-1/python-basics/output.py
+++ practice-1/python-basics/output.py
@@ -5,3 +5,13 @@ print('This will also work!')
 # Output without a new line
 print("Hello World!", end=" ")
 print("I will print on the same line.")
+
+# Output numbers
+print(3)
+print(358)
+print(50000)
+print(3 + 3)
+print(2 * 5)
+
+# Mixed output
+print("I am", 35, "years old.")
