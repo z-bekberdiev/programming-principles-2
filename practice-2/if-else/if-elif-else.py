c2768c1a72fd50a9f5208a982e960c87fd5c358f
Zhantore Bekberdiev <z_bekberdiev@icloud.com>
2026-01-27 14:04:02 +0500

    Added 'if-elif-else.py' file.

---
+++ practice-2/if-else/if-elif-else.py
@@ -0,0 +1,29 @@
+# Simple if-elif condition
+a = 33
+b = 33
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+
+# Multiple elif statements
+score = 75
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")
+elif score >= 60:
+    print("Grade: D")
+
+# Simple if-elif-else condition
+a = 200
+b = 33
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+else:
+    print("a is greater than b")
+ 
