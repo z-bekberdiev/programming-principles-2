7224ed1b662d2168eb27ce8d71e1cc3d7ed13a79
Zhantore Bekberdiev <z_bekberdiev@icloud.com>
2026-01-27 14:03:27 +0500

    Added 'if-statement.py' file.

---
+++ practice-2/if-else/if-statement.py
@@ -0,0 +1,18 @@
+# Simple if condition
+a = 33
+b = 200
+if b > a:
+    print("b is greater than a")
+
+# Multiple statements in if block
+age = 20
+if age >= 18:
+    print("You are an adult")
+    print("You can vote")
+    print("You have full legal rights")
+
+# Using variable in condition
+is_logged_in = True
+if is_logged_in:
+    print("Welcome back!")
+ 
