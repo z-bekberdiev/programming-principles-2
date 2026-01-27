203e63096e56dc6076ce5b52ba7b1967ef7b489d
Zhantore Bekberdiev <z_bekberdiev@icloud.com>
2026-01-27 14:02:17 +0500

    Added 'boolean-comparison.py' file.

---
+++ practice-2/boolean/boolean-comparison.py
@@ -0,0 +1,7 @@
+print(10 > 9)   # True (10 greater than 9)
+print(10 == 9)  # False (10 equal to 9)
+print(10 < 9)   # False (10 less than 9)
+print(10 >= 9)   # True (10 greater than or equal to 9)
+print(10 != 9)   # True (10 not equal to 9)
+print(10 <= 9)   # False (10 less than or equal to 9)
+ 
