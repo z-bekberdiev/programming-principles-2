aa6f58a3535e2af933fcaed99b3f9c89d52d0a84
Zhantore Bekberdiev <z_bekberdiev@icloud.com>
2026-01-27 14:06:40 +0500

    Added 'while-loop.py' file.

---
+++ practice-2/loops/while-loop.py
@@ -0,0 +1,14 @@
+# Simple while loop
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+
+# The same loop but with else
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print("i is no longer less than 6")
+ 
