237c4569378f871736c6e73fbb1990bc2acb3141
Zhantore Bekberdiev <z_bekberdiev@icloud.com>
2026-02-03 23:11:57 +0500

    Added 'iterators-basics.py' file.

---
+++ practice-4/iterators-generators/iterators-basics.py
@@ -0,0 +1,18 @@
+# tuple
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+# str
+mystr = "banana"
+myit = iter(mystr)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
