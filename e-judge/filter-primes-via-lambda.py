29b32cfe34cce88367af014ae788d75400f62b97
Zhantore Bekberdiev <z_bekberdiev@icloud.com>
2026-02-11 16:32:46 +0500

    Removed vulnerability from the code.

--- e-judge/filter-primes-via-lambda.py
+++ e-judge/filter-primes-via-lambda.py
@@ -10,4 +10,4 @@ array = list(filter((lambda number: number if is_prime(number) else 0), array))
 print(*array) if len(array) > 0 else print("No primes")
 
 # Solution No. 2
-print(*list(filter(lambda i: i if not [False for j in range(2, i) if i % j == 0] else 0, [int(i) for i in input().split() if int(i) != 1])))
+print(*list(filter(lambda i: i if not [False for j in range(2, i) if i % j == 0] else 0, [int(i) for i in input().split() if int(i) > 1])))
