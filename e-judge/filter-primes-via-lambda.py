b6dacb4aef7316d838a0a2df5b4d45aea075aa07
Zhantore Bekberdiev <z_bekberdiev@icloud.com>
2026-02-11 16:15:58 +0500

    Added other way to solve the problem.

--- e-judge/filter-primes-via-lambda.py
+++ e-judge/filter-primes-via-lambda.py
@@ -1,3 +1,4 @@
+# Solution No. 1
 def is_prime(number):
     for integer in range(2, number):
         if number % integer == 0:
@@ -7,3 +8,6 @@ def is_prime(number):
 array = [int(number) for number in input().split() if number != '1']
 array = list(filter((lambda number: number if is_prime(number) else 0), array))
 print(*array) if len(array) > 0 else print("No primes")
+
+# Solution No. 2
+print(*list(filter(lambda i: i if not [False for j in range(2, i) if i % j == 0] else 0, [int(i) for i in input().split() if int(i) != 1])))
