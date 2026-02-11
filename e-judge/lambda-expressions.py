ebcb9472e75ba229ccfceccf3244444ddff9fee9
Zhantore Bekberdiev <z_bekberdiev@icloud.com>
2026-02-11 23:21:35 +0500

    Added other way to solve the problem.

--- e-judge/lambda-expressions.py
+++ e-judge/lambda-expressions.py
@@ -1,3 +1,4 @@
+# Solution No. 1
 length = int(input())
 numbers = [int(element) for element in input().split()]
 count = int(input())
@@ -24,3 +25,8 @@ for operation in operations:
             numbers = list(map(lambda element: element ** int(operation[1]), numbers))
 
 print(*numbers)
+
+# Solution No. 2
+numbers = [[int(element) for element in input().split()] for _ in range(int(input()) * 0, 1)]
+for operation in [input().split() for _ in range(0, int(input()))]: numbers[0] = list(map(lambda element: abs(element) if operation[0] == "abs" else element + int(operation[1]) if operation[0] == "add" else element * int(operation[1]) if operation[0] == "multiply" else element ** int(operation[1]) if operation[0] == "power" else 0, numbers[0]))
+print(*numbers[0])
